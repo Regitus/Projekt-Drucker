6ca5528c6445ecf9b687d3231cc5392f3925ca5b
david <david_junghans@web.de>
2019-10-24 15:34:53 +0200

    proper

--- src/main/java/aufgaben/Properties.java
+++ src/main/java/aufgaben/Properties.java
@@ -3,7 +3,5 @@ package main.java.aufgaben;
 public class Properties {
 	public static int yMotorPort = 2;
 	public static int xMotorPort = 3;
-	public static int anzahl = 2;
-	public static int anzahl = 2;
-	public static int anzahl = 2;
+
 }
