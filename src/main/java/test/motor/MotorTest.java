463ecb4d22452a4921c04e6b19ca61ccda92f0d9
Regitus <hampl.rene@googlemail.com>
2019-11-07 10:07:11 +0100

    ff

--- src/main/java/test/motor/MotorTest.java
+++ src/main/java/test/motor/MotorTest.java
@@ -1,5 +1,6 @@
 package main.java.test.motor;
 
+import lejos.utility.Delay;
 import main.java.bo.factories.FactoryProvider;
 import main.java.bo.factories.FactoryTyp;
 import main.java.bo.factories.MotorFactory;
@@ -11,14 +12,15 @@ import main.java.bo.motoren.StiftMotor;
 public class MotorTest {
 
 	public static void main(String[] args) {
-		/*FactoryProvider factoryProvider = new FactoryProvider();
+		FactoryProvider factoryProvider = new FactoryProvider();
 		MotorFactory motorFactory = (MotorFactory) factoryProvider.getFactory(FactoryTyp.MotorFactory);
 		StiftMotor stiftMotor = (StiftMotor) motorFactory.create(MotorTyp.StiftMotor);
 		AchsenMotor yMotor = (AchsenMotor) motorFactory.create(MotorTyp.YAchsenMotor);
 		AchsenMotor xMotor = (AchsenMotor) motorFactory.create(MotorTyp.XAchsenMotor);
 		stiftMotor.positionaendern(Stellung.OBEN);
 		yMotor.positionaendern(1, 1);
-		xMotor.positionaendern(1, 1);*/
+		xMotor.positionaendern(1, 1);
+		Delay.msDelay(1000);
 	}
 
 }
