3e0a5024a9b590b441094f202dfb57e1a16a06ff
david <david_junghans@web.de>
2019-11-21 12:43:15 +0100

    changed stift functions

--- src/main/java/test/roboter/RoboterTest.java
+++ src/main/java/test/roboter/RoboterTest.java
@@ -1,63 +1,28 @@
 package main.java.test.roboter;
 
+import lejos.utility.Delay;
+import main.java.bo.factories.FactoryProvider;
+import main.java.bo.factories.FactoryTyp;
+import main.java.bo.factories.MotorFactory;
+import main.java.bo.factories.MotorTyp;
+import main.java.bo.motoren.Stellung;
+import main.java.bo.motoren.StiftMotor;
 import main.java.bo.roboter.DruckerRoboter;
 
 public class RoboterTest {
 	
 	public static void main(String[] args) {
-		DruckerRoboter roboter = new DruckerRoboter();
-		roboter.moveSync(4.07,2.96);
-		roboter.moveSync(4.23,3.23);
-		roboter.moveSync(4.33,3.53);
-		roboter.moveSync(4.44,3.8);
-		roboter.moveSync(4.57,4.17);
-		roboter.moveSync(4.84,4.47);
-		roboter.moveSync(5.17,4.67);
-		roboter.moveSync(5.48,4.87);
-		roboter.moveSync(5.81,5.14);
-		roboter.moveSync(6.18,5.31);
-		roboter.moveSync(6.52,5.34);
-		roboter.moveSync(6.85,5.34);
-		roboter.moveSync(7.19,5.34);
-		roboter.moveSync(7.46,5.31);
-		roboter.moveSync(7.83,5.14);
-		roboter.moveSync(8.1,4.84);
-		roboter.moveSync(8.16,4.54);
-		roboter.moveSync(8.16,4.27);
-		roboter.moveSync(8.03,4.0);
-		roboter.moveSync(7.69,3.63);
-		roboter.moveSync(7.32,3.36);
-		roboter.moveSync(6.99,3.29);
-		roboter.moveSync(6.72,3.23);
-		roboter.moveSync(6.48,3.56);
-		roboter.moveSync(6.28,3.93);
-		roboter.moveSync(6.22,4.23);
-		roboter.moveSync(6.15,4.6);
-		roboter.moveSync(6.12,4.94);
-		roboter.moveSync(6.12,5.24);
-		roboter.moveSync(6.15,5.51);
-		roboter.moveSync(6.18,5.81);
-		roboter.moveSync(6.18,6.08);
-		roboter.moveSync(6.05,6.38);
-		roboter.moveSync(5.75,6.62);
-		roboter.moveSync(5.48,6.75);
-		roboter.moveSync(5.17,6.79);
-		roboter.moveSync(4.87,6.79);
-		roboter.moveSync(4.6,6.79);
-		roboter.moveSync(4.91,6.69);
-		roboter.moveSync(5.31,6.69);
-		roboter.moveSync(5.71,6.69);
-		roboter.moveSync(6.05,6.69);
-		roboter.moveSync(6.32,6.59);
-		roboter.moveSync(6.65,6.59);
-		roboter.moveSync(7.02,6.59);
-		roboter.moveSync(7.36,6.62);
-		roboter.moveSync(7.63,6.69);
-		roboter.moveSync(7.93,6.75);
-		roboter.moveSync(8.23,6.75);
-		roboter.moveSync(8.53,6.75);
-		roboter.moveSync(8.8,6.75);
-
+		
+		FactoryProvider factoryProvider = new FactoryProvider();
+		MotorFactory motorFactory = (MotorFactory) factoryProvider.getFactory(FactoryTyp.MotorFactory);
+		StiftMotor stiftMotor = (StiftMotor) motorFactory.create(MotorTyp.StiftMotor);
+		
+		for(int i = 0; i<30; i++) {
+			stiftMotor.positionaendern(Stellung.UNTEN);
+			stiftMotor.positionaendern(Stellung.OBEN);
+		}
+	
+		
 	}
 
 }
