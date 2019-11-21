16eff81af2f21469639da1b4e7efadfbfcd333f3
david <david_junghans@web.de>
2019-11-21 09:24:51 +0100

    21.11 test

--- src/main/java/codeCreator/SwingPaint.java
+++ src/main/java/codeCreator/SwingPaint.java
@@ -47,3 +47,5 @@ public class SwingPaint {
         frame.setVisible(true);
     }
 }
+
+
--- src/main/java/test/roboter/RoboterTest.java
+++ src/main/java/test/roboter/RoboterTest.java
@@ -6,14 +6,14 @@ public class RoboterTest {
 	
 	public static void main(String[] args) {
 		DruckerRoboter roboter = new DruckerRoboter();
-		/*roboter.move(2, 2);
-		roboter.move(4, 0);
-		roboter.move(3,  1);
-		roboter.move(1, 1);*/
-		roboter.moveSync(2, 2);
+//		roboter.move(0, 4);
+//		roboter.move(4, 4);
+		//roboter.move(4,  0);
+		//roboter.move(0, 0);
+		roboter.moveSync(0, 4);
+		roboter.moveSync(4, 4);
 		roboter.moveSync(4, 0);
-		roboter.moveSync(3, 1);
-		roboter.moveSync(1, 1);
+		roboter.moveSync(0, 0);
 		
 	}
 
